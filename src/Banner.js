f11c01ac37d9cbbe57c2097ecdbbd950220b8174
jaehokim12 <62605981+jaehokim12@users.noreply.github.com>
2021-06-01 02:47:10 +0900

    06/01 publish

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests';
 import './Banner.css';
+
 function Banner() {
   const [movie, setMovie] = useState([]);
   useEffect(() => {
@@ -24,7 +25,7 @@ function Banner() {
     <header
       className="banner"
       style={{
-        bacgroundSize: 'cover',
+        backgroundSize: 'cover',
         backgroundImage: `url(
             "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
         )`,
@@ -32,7 +33,9 @@ function Banner() {
       }}
     >
       <div className="banner__contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
@@ -41,6 +44,8 @@ function Banner() {
           {truncate(movie?.overview, 150)}
         </h1>
       </div>
+
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
